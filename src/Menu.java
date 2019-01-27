a408f5531c57476f33179360d070c92cb11ebede
DoloresDai <841856753@qq.com>
2019-01-28 07:06:25 +0800

    Set values for orders.

--- src/Menu.java
+++ src/Menu.java
@@ -7,4 +7,33 @@ public class Menu {
     int sumHalfPrice;
     int sumLessPrice;
     StringBuffer halfItems = new StringBuffer();
+
+    Menu(String[] idArr, int[] numArr) {
+        for (int j = 0; j < idArr.length; j++) {
+            Items item = new Items(idArr[j], numArr[j]);
+            item.getItems();
+            item.isHalf();
+            item.countOriginalPrice();
+            item.countHalfPrice();
+            Tool.print(item.name + " x " + item.num + " = " + String.valueOf(item.price * item.num) + "元");
+            this.sumOriginalPrice += item.originalPrice;
+            this.sumHalfPrice += item.halfPrice;
+            if (item.sale) {
+                try {
+                    halfItems = halfItems.append(item.name + ",");
+                } catch (NullPointerException e) {
+                    Tool.print("空指针" + e);
+                }
+            }
+        }
+        try {
+            if (!halfItems.equals("")) {
+                halfItems.setLength(halfItems.length() - 1);
+            }
+        } catch (NullPointerException e) {
+            Tool.print("空指针" + e);
+        }
+        sumLessPrice = countLessPrice(sumOriginalPrice);
+    }
 }
+
