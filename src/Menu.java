9b779d65b592b0ce411d28e02bc1cc3cf973509a
DoloresDai <841856753@qq.com>
2019-01-28 07:05:16 +0800

    Initialize Menu class

---
+++ src/Menu.java
@@ -0,0 +1,10 @@
+/***
+ * This is a menu class, used to collect information about all dishes,
+ * and can automatically generate the original price, preferential price.
+ */
+public class Menu {
+    int sumOriginalPrice;
+    int sumHalfPrice;
+    int sumLessPrice;
+    StringBuffer halfItems = new StringBuffer();
+}
