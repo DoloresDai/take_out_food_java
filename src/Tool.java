5216a56360e1bbf780bf51403f7cfed6b0b3c7b3
DoloresDai <841856753@qq.com>
2019-01-28 07:03:32 +0800

    Add comments to tool classes.

--- src/Tool.java
+++ src/Tool.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 
+/***
+ * This is a tool class that encapsulates common methods.
+ * @author Dolores
+ * @version 1.0
+ */
 public class Tool {
+    /**
+     * @param arr Any string you want to print；
+     * @return null
+     */
     static void print(String arr) {
         System.out.println(arr);
     }
 
+    /**
+     * @param numStringArr - Numbers stored in strings;
+     * @return - Array converted to numerical form
+     */
     static int[] convertToIntArr(String[] numStringArr) {
         int[] numArr = new int[numStringArr.length];
         for (int i = 0; i < numStringArr.length; i++) {
@@ -13,6 +26,10 @@ public class Tool {
         return numArr;
     }
 
+    /**
+     * @param - Console input string
+     * @return String
+     */
     static Scanner getControlStatic() {
         return new Scanner(System.in);
     }
